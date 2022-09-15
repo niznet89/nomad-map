c1bbf061146ee69d0723756a2903680fcc0d59fa
Tenzin Rose <tenzinrose@gmail.com>
2022-09-15 17:39:34 -0300

    Functions adjusted, still needs work

--- contracts/contracts/NomadMap.sol
+++ contracts/contracts/NomadMap.sol
@@ -5,18 +5,32 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NomadMap is ERC20 {
 
-  struct cities {
-    string location;
-    string[] WaGroupNames;
-    string[] WaUrls;
+  struct City {
+    string name;
+    string country;
+    mapping(string => int8) WaGroupNames;
+    mapping(string => string) WaUrls;
+    string[] WaGroupNamesArray;
   }
 
   address[] members;
+  City[] cities;
 
   constructor() ERC20("NomadMap", "DMM") {
     }
 
   function addMember() public {
+    // Payable?
     members.push(msg.sender);
+    // User gets 3 DMM tokens once he signs up
+    _mint(msg.sender, 3000000000000000000);
+  }
+
+  function createCity(string calldata _name, string calldata _country) public {
+    // Does the city need to be an argument?
+    City newCity = City();
+    newCity.name = _name;
+    newCity.country = _country;
+
   }
 }
