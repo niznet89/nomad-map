c0dd93d9d27c6abb4af8839f263a37eae91127a0
Tenzin Rose <tenzinrose@gmail.com>
2022-09-30 16:49:20 -0300

    Added a map to the page; starting to flesh it out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+
+  console.log(import.meta.env);
+  const position = [51.505, -0.09];
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ minHeight: "100vh", minWidth: "100vw" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default App
---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useMap = () => {
+  // Logic goes here
+  const [position, setPosition] = useState({
+    lat: 47.21725,
+    lng: -1.55336,
+  })
+};
