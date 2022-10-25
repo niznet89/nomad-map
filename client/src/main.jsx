69f46d999d2ed0b3dee71d16055fcaceaff91796
Tenzin Rose <tenzinrose@gmail.com>
2022-10-25 12:48:58 -0500

    Changing React leaflet CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import "leaflet/dist/leaflet.css";
+//import "leaflet/dist/leaflet.css";
 import SearchAppBar from "./SearchAppBar.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
