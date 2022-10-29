1512dcafffbfb70df4abf34e487caa1a3e83e137
Tenzin Rose <tenzinrose@gmail.com>
2022-10-28 20:18:47 -0500

    Adding Hardhat info

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    arbitrum: {
+      url: `${process.env.ALCHEMY_ARBITRUM_URL}`,
+      accounts: [`0x${process.env.ALCHEMY_ARBITRUM_KEY}`],
+    }
+  }
 };
