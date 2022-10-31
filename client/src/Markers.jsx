152a55242289e251b163e8e9c9e9d08d687a0897
Tenzin Rose <tenzinrose@gmail.com>
2022-10-31 15:07:07 -0500

    Testing out local deploy of data from SC

--- client/src/Markers.jsx
+++ client/src/Markers.jsx
@@ -1,10 +1,25 @@
 import { Marker, Popup } from "react-leaflet";
 import nomadGroups from "./assets/nomad-groups.json";
 import { useState, useEffect, useCallback } from "react";
+import { ethers } from "ethers";
+import  nomadMap  from "./assets/nomadMap.json";
+
 
 export default function Markers() {
-  const [groups, setGroups] = useState(nomadGroups.features);
-  console.log(groups[0])
+  const [groups, setGroups] = useState(['Chiang Mai', '98.9931284', '18.787747', [], ['https://www.facebook.com/groups/cmnomads/']]);
+  console.log(groups[0]);
+
+  async function pullContractData() {
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
+    const contract = new ethers.Contract("0xFD471836031dc5108809D173A067e8486B9047A3", nomadMap.abi, provider);
+    const locations = contract.returnAllCities();
+    setGroups(locations);
+
+  }
+
+  pullContractData();
+
+
 
   return (
 
@@ -13,14 +28,16 @@ export default function Markers() {
       <>
       <Marker
           position={[
-            group.geometry.coordinates[1],
-            group.geometry.coordinates[0]
+            group[2],
+            group[1]
           ]}
         >
           <Popup>
-            <b>{group.properties.name} </b>
+            <b>{group[1]} </b>
             <br />
-            {group.properties.description}</Popup>
+            {group[4].map((element, i) => {
+               <b>element</b>
+            })}</Popup>
       </Marker>
       </>
       )
